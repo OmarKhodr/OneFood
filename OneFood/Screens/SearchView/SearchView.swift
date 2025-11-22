4e6b8db7d7640c49745c1f4f90fc97a00879cba8
Abd Rahman khodr <abdo.khd2002@gmail.com>
2025-11-22 22:56:20 +0400

    New Changes

---
+++ OneFood/Screens/SearchView/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  OneFood
+//
+//  Created by Abdel Rahman Khodr on 22/11/2025.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ OneFood/Views/RestaurantHorizontalGrid.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantHorizontalGrid.swift
+//  OneFood
+//
+//  Created by Abdel Rahman Khodr on 22/11/2025.
+//
+
+import SwiftUI
+
+struct RestaurantHorizontalGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantHorizontalGrid()
+}
