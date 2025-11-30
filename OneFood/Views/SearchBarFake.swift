c0f4bcd556454c0f1319e10c364066030e5773d0
Abd Rahman khodr <abdo.khd2002@gmail.com>
2025-11-30 12:22:54 +0400

    Refactoring HomeView to minimize code: - Created new SearchBarFake view - Added "title" parameter to RestaurantHorizontalGrid

---
+++ OneFood/Views/SearchBarFake.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarFake.swift
+//  OneFood
+//
+//  Created by Abdel Rahman Khodr on 30/11/2025.
+//
+
+import SwiftUI
+
+struct SearchBarFake: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarFake()
+}
